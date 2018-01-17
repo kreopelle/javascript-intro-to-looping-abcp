3ff452ecdbfe3bf04a3bea128138135260177544
Kayla Reopelle <kreopelle@gmail.com>
2018-01-17 19:26:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,3 +17,9 @@ function whileLoop(n) {
   return "done";
 }
 
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5;
+  }
+  while(array.length > 0 && maybeTrue())
+}
